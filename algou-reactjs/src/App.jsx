aec372ce9814916f948c810ef7528b081098ba19
nitish kumar ngh <nitishngh2206@gmail.com>
2025-05-19 10:28:57 +0530

    initial

---
+++ algou-reactjs/src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Components/Footer"
+import Navbar from "./Components/Navbar"
+
+
+function App() {
+
+
+  return (
+  // <h1 className ="text-3xl font-bold underline text-orange-500">Hello world</h1>
+  <Footer/>
+  )
+}
+
+export default App
---
+++ algou-reactjs/src/Bhavesh.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bhavesh = () => {
+  return (
+    <div>
+      <h1>Bhavesh</h1>
+    </div>
+  )
+}
+
+export default Bhavesh
